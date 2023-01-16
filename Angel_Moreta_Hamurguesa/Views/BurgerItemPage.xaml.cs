c0335a1183d162a8a0a77135e85ec86d9853e23f
AndresMoreta20 <andresmoreta2020@gmail.com>
2023-01-16 10:55:46 -0500

    Se actualiza Item

--- Angel_Moreta_Hamurguesa/Views/BurgerItemPage.xaml.cs
+++ Angel_Moreta_Hamurguesa/Views/BurgerItemPage.xaml.cs
@@ -1,14 +1,21 @@
 using Angel_Moreta_Hamburguesa.Models;
 using Angel_Moreta_Hamburguesa.Data;
 namespace Angel_Moreta_Hamburguesa.Views;
+
+[QueryProperty("Item", "Item")]
 public partial class BurgerItemPage : ContentPage
 {
-    Burger Item = new Burger();
+    //Burger Item = new Burger();
     bool _flag = false;
 	public BurgerItemPage()
 	{
 		InitializeComponent();
 	}
+    public Burger Item
+ {
+        get => BindingContext as Burger;
+        set => BindingContext = value;
+    }
 
     private void OnCheckBoxCheckedChanged(object sender, CheckedChangedEventArgs e)
     {
@@ -17,11 +24,12 @@ public partial class BurgerItemPage : ContentPage
 
     private async void OnSaveClicked(object sender, EventArgs e)
     {
-        Item.Name = nameB.Text;
-        Item.Description = descB.Text;
-        Item.WithExtraCheese = _flag;
-        App.BurgerRepo.AddNewBurger(Item);
+        /* Item.Name = nameB.Text;
+         Item.Description = descB.Text;
+         Item.WithExtraCheese = _flag;
+         App.BurgerRepo.AddNewBurger(Item);*/
         //Shell.Current.GoToAsync("..");
+        App.BurgerRepo.AddNewBurger(Item);
         await Shell.Current.GoToAsync(nameof(BurgerListPage));
 
     }
